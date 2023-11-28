bb7a09d60d34826c57022be4ec781c35f472bbc5
AlgorithmicsCarlos <152320969+AlgorithmicsCarlos@users.noreply.github.com>
2023-11-28 19:37:20 +0100

    Add files via upload

---
+++ script1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Saca por la consola la frase "Hello World"
+        print("Hello world");
+        //Saca por la consola el numero 7
+        print(7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
